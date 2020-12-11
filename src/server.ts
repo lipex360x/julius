0a45cc721c772373d0e67bbdae274c3bb3ecc68b
Felipe Borges <lipex360@gmail.com>
2020-12-11 20:38:09 -0300

    restart julius

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'
-
-const server = app.listen(3000, () => console.log(`🚀 App Started at Port 3000`))
-
-process.on('SIGINT', () => {
-  server.close()
-  console.log(`🔒 App Stopped`)
-})
