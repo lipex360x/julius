365d5fac38961aec2bbc5e179f3f1e8e7c8b6bee
Felipe Borges <lipex360@gmail.com>
2020-12-29 01:08:13 -0300

    server updated

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const server = app.listen(3333, () => console.log('🚀 API Started at Port 3333 🚀'))
+const server = app.listen(3333, () => console.log('🚀 API Started at Port 3333'))
 
 process.on('SIGINT', () => {
   server.close()
