5621bd2f3bf4dd2d33babff4a7fd43581f3280df
Felipe Borges <lipex360@gmail.com>
2021-01-05 23:33:18 -0300

    Header updated

--- frontend/reactjs/src/components/Header/index.tsx
+++ frontend/reactjs/src/components/Header/index.tsx
@@ -7,7 +7,13 @@ type HeaderProps = PropsWithChildren<{
   title: string
 }>
 
-export default function Header({ title, children }: HeaderProps) {
+/**
+ * Main Header of Application
+ * @param {string} title - Title of Component
+ * @param {string} children - Text of Component
+ * @returns {JSX.Element} JSX
+**/
+export default function Header({ title, children }: HeaderProps): JSX.Element {
   return (
     <S.Section>
       
